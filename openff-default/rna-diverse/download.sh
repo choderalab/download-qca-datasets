bd2a764c940857e45094ae757ecc1c3144c0eb79
kt <kntkb.qbr@gmail.com>
2023-03-10 16:20:05 -0500

    add openff-default rna-diverse

---
+++ openff-default/rna-diverse/download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#BSUB -P "rna"
+#BSUB -J "rna-dl"
+#BSUB -n 1
+#BSUB -R rusage[mem=64]
+#BSUB -R span[hosts=1]
+#BSUB -q cpuqueue
+#BSUB -sp 1 # low priority. default is 12, max is 25
+#BSUB -W 12:00
+#BSUB -o out_%J_%I.stdout
+#BSUB -eo out_%J_%I.stderr
+#BSUB -L /bin/bash
+
+
+source ~/.bashrc
+
+
+# change dir
+echo "changing directory to ${LS_SUBCWD}"
+cd $LS_SUBCWD
+
+conda activate espaloma
+/bin/time --verbose python downloader-openff-default.py
+echo done
